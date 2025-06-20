db887f397c8ba0de42142ccef22e66c90cb0678c
Darshan Parikh <darshan1191@gmail.com>
2025-06-20 00:39:37 -0400

    Solution for Implement Trie

---
+++ Trie.java
@@ -0,0 +1,69 @@
+/**
+ * Leetcode 208. Implement Trie (Prefix Tree)
+ * Link: https://leetcode.com/problems/implement-trie-prefix-tree/description/
+ */
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * TC: O(L) L -> Length of the word
+     * SC: O(26L) for 1 word. If there are total N words worst case O(26NL). 26 can be ignored as constant
+     */
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (char c: word.toCharArray()) {
+            int idx = c - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+
+            curr = curr.children[idx];
+        }
+        curr.isWord = true;
+    }
+
+    /**
+     * TC: O(L) L -> Length of the word
+     * SC: O(1) there is no extra auxiliary space being used to search on Trie
+     */
+    public boolean search(String word) {
+        TrieNode lastNode = getTrieNodeWith(word);
+        return lastNode != null && lastNode.isWord;
+    }
+
+    /**
+     * TC: O(L) L -> Length of the word
+     * SC: O(1) there is no extra auxiliary space being used to search on Trie
+     */
+    public boolean startsWith(String prefix) {
+        return getTrieNodeWith(prefix) != null;
+    }
+
+    private TrieNode getTrieNodeWith(String prefix) {
+        TrieNode curr = root;
+        for(char c: prefix.toCharArray()) {
+            int idx = c - 'a';
+
+            if (curr.children[idx] == null) {
+                return null;
+            }
+
+            curr = curr.children[idx];
+        }
+        return curr;
+    }
+}
+
+class TrieNode {
+    boolean isWord;
+    TrieNode[] children;
+
+    public TrieNode() {
+        this.children = new TrieNode[26];
+    }
+}
